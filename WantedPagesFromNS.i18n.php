8c866f7ebd52d4b8b60f8d351c31dbbe8609116f
Christian Aistleitner <christian@quelltextlich.at>
2013-08-12 11:45:22 +0200

    Initial import of 1.0.0_beta release
    The imported zip was taken from
      http://kazekr.net/inne/WantedPagesFromNS_1.0.0_beta.zip

---
+++ WantedPagesFromNS.i18n.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Internationalization file for WantedPagesFromNS extension.
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$messages = array();
+
+/** English
+ * @author Kazimierz Król
+ */
+$messages['en'] = array(
+ 'wpfromns-desc'  => 'Wanted pages from a namespace',
+ 'wpfromns-links' => '{{PLURAL:$1|1 link|$1 links}}',
+ 'wpfromns-nons'  => 'WantedPagesFromNS: no namespace specified!',
+ 'wpfromns-nores' => 'No pages to display.',
+);
+
+/** Polish (Polski)
+ * @author Kazimierz Król
+ */
+$messages['pl'] = array(
+ 'wpfromns-desc'  => 'Lista potrzebnych stron z przestrzeni nazw',
+ 'wpfromns-links' => '{{PLURAL:$1|1 link|$1 linki|$1 linków}}',
+ 'wpfromns-nons'  => 'WantedPagesFromNS: nie podano przestrzeni nazw (namespace)!',
+ 'wpfromns-nores' => 'Brak stron do wyświetlenia.',
+);
---
+++ WantedPagesFromNS.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ WantedPagesFromNS v1.0.0 beta -- Shows list of wanted page from specified namespace
+
+ Author: Kazimierz Król
+
+ Code based largely on DPL Forum extension by Ross McClure
+ http://www.mediawiki.org/wiki/User:Algorithm
+
+ This program is free software; you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation; either version 2 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License along
+ with this program; if not, write to the Free Software Foundation, Inc.,
+ 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ http://www.gnu.org/copyleft/gpl.html
+
+ To install, add following to LocalSettings.php
+   include_once("$IP/extensions/WantedPagesFromNS/WantedPagesFromNS.php");
+
+*/
+
+if ( !defined( 'MEDIAWIKI' ) ) {
+  echo( "This file is an extension to the MediaWiki software and is not a valid access point" );
+  die( 1 );
+}
+
+$wgExtensionFunctions[] = 'efWantedPagesFromNSInit';
+$wgExtensionCredits['parserhook'][] = array(
+  'path' => __FILE__,
+  'name' => 'WantedPagesFromNS',
+  'author' => 'Kazimierz Król',
+  'version' => '1.0.0 beta',
+  'url' => '',
+  'description' => 'Wanted pages from a namespace',
+  'descriptionmsg' => 'wpfromns-desc',
+);
+
+$dir = dirname( __FILE__ ) . '/';
+$wgExtensionMessagesFiles['WantedPagesFromNS'] = $dir . 'WantedPagesFromNS.i18n.php';
+$wgAutoloadClasses['WantedPagesFromNS'] = $dir . 'WantedPagesFromNS.body.php';
+
+function efWantedPagesFromNSInit() {
+  global $wgParser;
+
+  wfLoadExtensionMessages( 'WantedPagesFromNS' );
+  $wgParser->setHook( 'wantedpagens', 'PageListRender' );
+  //$wgParser->setFunctionHook( 'forumlink', array( new DPLForum(), 'link' ) );
+  return TRUE;
+}
+
+function PageListRender( $input, array $args, Parser $parser, PPFrame $frame ) {
+
+  $f = new WantedPagesFromNS();
+  return $f->parse( $input, $parser );
+}
